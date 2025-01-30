c63345d2433ba514003a19088241dc72efe3e968
juanjo0824 <juandelgado1@colegioguanenta.edu.co>
2025-01-30 17:50:22 -0500

    se agrego el programa en python

---
+++ suma_n_numeros.py
@@ -0,0 +1,14 @@
+# input
+print("#######################")
+n= input("dijite su numero ")
+print("#######################")
+n=int(n)
+
+
+# formula
+s = (n * (n+1))/2
+
+#output 
+print("#############################")
+print("la suma de los numeros" , s)
+print("#############################")
